af184520aa30fb357e1a3ea6b08bb2cb3d0f841f
Luca Wehrstedt <lcw@fb.com>
2020-12-04 08:11:32 -0800

    Make CUDAFuture remember and restore current device in callback
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/48789
    
    CUDAFuture aims to "capture" the current state of CUDA-related stuff when the future is marked complete (e.g., by looking at current streams and recording events on them) and then "replicate" a similar state when users synchronize with the result of the future (by synchronizing the current streams with these events).
    
    However, one "contextual" aspect of CUDA that we weren't capturing/replicating was the current device. This diff tries to fix that. I must mention that we can only do this for callbacks, while we cannot do it for the wait() method. I don't know if such a discrepancy between the two actually makes the overall behavior _worse_. I'd love to hear people's opinions on this.
    ghstack-source-id: 117868896
    
    Differential Revision: [D25210335](https://our.internmc.facebook.com/intern/diff/D25210335/)

--- aten/src/ATen/cuda/CUDAFuture.h
+++ aten/src/ATen/cuda/CUDAFuture.h
@@ -31,6 +31,8 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
   }
 
   void postMarkCompletedHook(const at::IValue& value) override {
+    currentDevice_ = c10::cuda::current_device();
+
     // Extract them once and cache them for later uses.
     dataPtrs_ = extractDataPtrs(value);
 
@@ -85,6 +87,8 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
         }
       }
 
+      c10::cuda::CUDAGuard deviceGuard(currentDevice_);
+
       callback();
     };
   }
@@ -109,6 +113,10 @@ struct TORCH_CUDA_API CUDAFuture : at::ivalue::Future {
   // Once WorkNCCL is gone (as part of the Future and Work merge) this should be
   // fixed.
  protected:
+  // The device that was current when markCompleted was called, which we'll
+  // restore when invoking callbacks.
+  c10::DeviceIndex currentDevice_;
+
   std::shared_ptr<std::vector<at::cuda::CUDAEvent>> cudaEvents_;
   std::vector<std::reference_wrapper<const at::DataPtr>> dataPtrs_;
 
